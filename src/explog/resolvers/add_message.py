ca8198c545012f12ad76b9ad5d2aa5ef929ad9dd
Russell Owen <rowen@uw.edu>
2020-11-05 12:54:47 -0800

    Update code and test registry for new daf_butler

--- src/explog/resolvers/add_message.py
+++ src/explog/resolvers/add_message.py
@@ -56,24 +56,25 @@ async def add_message(
 
     # Check obs_id and determine day_obs.
     loop = asyncio.get_running_loop()
-    exposure_start_time = await loop.run_in_executor(
+    obs_id = data_dict["obs_id"]
+    day_obs = await loop.run_in_executor(
         None,
-        get_start_time_from_registries,
+        get_day_obs_from_registries,
         registries,
-        data_dict["obs_id"],
+        obs_id,
         data_dict["instrument"],
     )
-    if exposure_start_time is None:
+    if day_obs is None:
         if is_new:
             exposure_start_time = curr_tai
+            day_obs_full = exposure_start_time - 12 * u.hr
+            day_obs = int(day_obs_full.strftime("%Y%m%d"))
         else:
-            obs_id = data_dict["obs_id"]
             raise RuntimeError(
                 f"Exposure {obs_id} not found and is_new is false"
             )
 
-    obs_day_full = exposure_start_time - 12 * u.hr
-    data_dict["day_obs"] = int(obs_day_full.strftime("%Y%m%d"))
+    data_dict["day_obs"] = day_obs
 
     # Add the message.
     async with exposure_log_database.engine.acquire() as connection:
@@ -90,10 +91,10 @@ async def add_message(
 ButlerList = typing.Sequence[lsst.daf.butler.Butler]
 
 
-def get_start_time_from_registries(
+def get_day_obs_from_registries(
     registries: ButlerList, obs_id: str, instrument: str
-) -> astropy.time.Time:
-    """Get the obs_day of a specified exposure, or None if not found.
+) -> int or None:
+    """Get the day of observation of an exposure, or None if not found.
 
     Parameters
     ----------
@@ -105,16 +106,24 @@ def get_start_time_from_registries(
     instrument
         Instrument name.
 
+    Returns
+    -------
+    day_obs : `int` or `None`
+        The day of observation of the exposure, if found, else None.
+
     Notes:
     -----
     If more than one matching exposure is found,
     silently uses the first one.
     """
-    query_str = f"exposure.name = '{obs_id}' and instrument='{instrument}'"
-    for registry in registries:
-        records = list(
-            registry.queryDimensionRecords("exposure", where=query_str)
-        )
-        if records:
-            return records[0].timespan.begin
+    try:
+        query_str = f"exposure.obs_id='{obs_id}' and instrument='{instrument}'"
+        for registry in registries:
+            records = list(
+                registry.queryDimensionRecords("exposure", where=query_str)
+            )
+            if records:
+                return records[0].day_obs
+    except Exception as e:
+        print(f"Error in butler query: {e}")
     return None
