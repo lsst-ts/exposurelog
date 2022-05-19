eee7f954a81acfbc4aaee0495c8621493d0a3316
Russell Owen <rowen@uw.edu>
2022-05-19 11:18:03 -0700

    Only migrate the message table if it exists

--- alembic/versions/5fcfc236c8df_add_levels_and_urls_columns.py
+++ alembic/versions/5fcfc236c8df_add_levels_and_urls_columns.py
@@ -4,6 +4,9 @@ Revision ID: 5fcfc236c8df
 Revises: 3bb3cd14b2dd
 Create Date: 2022-03-18 16:56:58.689843
 """
+import logging
+import typing
+
 import sqlalchemy as sa
 import sqlalchemy.types as saty
 
@@ -15,18 +18,33 @@ down_revision = "3bb3cd14b2dd"
 branch_labels = None
 depends_on = None
 
+MESSAGE_TABLE_NAME = "message"
 
-def upgrade():
-    op.add_column("message", sa.Column("level", saty.Integer(), nullable=True))
+
+def upgrade(log: logging.Logger, table_names: typing.Set[str]) -> None:
+    if MESSAGE_TABLE_NAME not in table_names:
+        log.info(f"No {MESSAGE_TABLE_NAME} table; nothing to do")
+        return
+    log.info("Add 'level' and 'urls' columns")
     op.add_column(
-        "message", sa.Column("urls", saty.ARRAY(sa.Text), nullable=True)
+        MESSAGE_TABLE_NAME, sa.Column("level", saty.Integer(), nullable=True)
     )
-    op.execute("UPDATE message SET level = 20")  # info
-    op.execute("UPDATE message SET urls = '{ }'")
-    op.alter_column("message", "level", nullable=False)
-    op.alter_column("message", "urls", nullable=False)
+    op.add_column(
+        MESSAGE_TABLE_NAME,
+        sa.Column("urls", saty.ARRAY(sa.Text), nullable=True),
+    )
+    op.execute(f"UPDATE {MESSAGE_TABLE_NAME} SET level = 20")  # 20=info
+    # "{ }" is Postgres syntax for an empty list
+    op.execute(f"UPDATE {MESSAGE_TABLE_NAME} SET urls = '{{ }}'")
+    op.alter_column(MESSAGE_TABLE_NAME, "level", nullable=False)
+    op.alter_column(MESSAGE_TABLE_NAME, "urls", nullable=False)
 
 
-def downgrade():
-    op.drop_column("message", "level")
-    op.drop_column("message", "urls")
+def downgrade(log: logging.Logger, table_names: typing.Set[str]) -> None:
+    if MESSAGE_TABLE_NAME not in table_names:
+        log.info(f"No {MESSAGE_TABLE_NAME} table; nothing to do")
+        return
+
+    log.info("Drop 'level' and 'urls' columns")
+    op.drop_column(MESSAGE_TABLE_NAME, "level")
+    op.drop_column(MESSAGE_TABLE_NAME, "urls")
